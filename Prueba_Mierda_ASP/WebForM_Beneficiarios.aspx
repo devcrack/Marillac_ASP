019239147c790c2a25294d06c7dbe459285dca5b
mvaldesb <mau_v_b20@hotmail.com>
2016-12-07 03:17:58 -0600

    Insertar en beneficiario

--- Prueba_Mierda_ASP/WebForM_Beneficiarios.aspx
+++ Prueba_Mierda_ASP/WebForM_Beneficiarios.aspx
@@ -89,7 +89,8 @@
             position: absolute;
             right: 1063px;
             width: 166px;
-            bottom: 208px;
+            bottom: 187px;
+            height: 37px;
         }
         .auto-style12 {
             z-index: 1;
@@ -131,7 +132,6 @@
             left: 222px;
             top: 292px;
             position: absolute;
-            right: 973px;
         }
         .auto-style18 {
             z-index: 1;
@@ -142,30 +142,28 @@
         }
         .auto-style19 {
             z-index: 1;
-            left: 219px;
-            top: 314px;
+            left: 223px;
+            top: 324px;
             position: absolute;
-            width: 186px;
+            width: 174px;
         }
         .auto-style20 {
             z-index: 1;
             left: 59px;
-            top: 351px;
+            top: 372px;
             position: absolute;
-            right: 1112px;
-        }
+            }
         .auto-style21 {
             z-index: 1;
             left: 171px;
-            top: 351px;
+            top: 373px;
             position: absolute;
-            right: 1014px;
             margin-top: 0px;
         }
         .auto-style22 {
             z-index: 1;
             left: 279px;
-            top: 351px;
+            top: 374px;
             position: absolute;
             right: 906px;
             margin-top: 0px;
@@ -173,21 +171,22 @@
         .auto-style23 {
             z-index: 1;
             left: 59px;
-            top: 384px;
+            top: 405px;
             position: absolute;
         }
         .auto-style24 {
             z-index: 1;
             left: 171px;
-            top: 383px;
+            top: 406px;
             position: absolute;
-            right: 1014px;
+            right: 1039px;
             margin-top: 0px;
+            bottom: 177px;
         }
         .auto-style25 {
             z-index: 1;
             left: 279px;
-            top: 383px;
+            top: 406px;
             position: absolute;
             right: 906px;
             margin-top: 0px;
@@ -195,21 +194,22 @@
         .auto-style26 {
             z-index: 1;
             left: 59px;
-            top: 417px;
+            top: 437px;
             position: absolute;
             right: 1126px;
         }
         .auto-style27 {
             z-index: 1;
             left: 171px;
-            top: 417px;
+            top: 437px;
             position: absolute;
             margin-top: 0px;
+            height: 25px;
         }
         .auto-style28 {
             z-index: 1;
             left: 279px;
-            top: 417px;
+            top: 437px;
             position: absolute;
             right: 906px;
             width: 100px;
@@ -237,25 +237,25 @@
             <asp:Label ID="Label6" runat="server" BorderStyle="None" CssClass="auto-style6" Height="16px" Text="Apellido materno: "></asp:Label>
             <asp:Label ID="Label7" runat="server" BorderStyle="None" CssClass="auto-style8" Height="16px" Text="Colonia: "></asp:Label>
             <asp:Label ID="Label8" runat="server" BorderStyle="None" CssClass="auto-style9" Height="16px" Text="Teléfono: "></asp:Label>
-            <asp:Label ID="Label9" runat="server" BorderStyle="None" CssClass="auto-style11" Height="16px" Text="Fecha de nacimiento: "></asp:Label>
+            <asp:Label ID="Label9" runat="server" BorderStyle="None" CssClass="auto-style11" Text="Fecha de nacimiento: (mm-dd-yyyy)"></asp:Label>
             <asp:Label ID="Label10" runat="server" BorderStyle="None" CssClass="auto-style10" Height="16px" Text="Sexo: "></asp:Label>
             <asp:TextBox ID="materno" runat="server" style="margin-top: 0px" CssClass="auto-style13" TabIndex="3"></asp:TextBox>
-            <asp:TextBox ID="fechaNacimiento" runat="server" style="margin-top: 0px" CssClass="auto-style19" TabIndex="9"></asp:TextBox>
+            <asp:TextBox ID="fechaNacimiento" runat="server" style="margin-top: 0px" CssClass="auto-style19" TabIndex="9" AutoPostBack="True" OnTextChanged="fechaNacimiento_TextChanged"></asp:TextBox>
             <asp:TextBox ID="direccion" runat="server" style="margin-top: 0px" CssClass="auto-style14" TabIndex="4"></asp:TextBox>
             <asp:TextBox ID="colonia" runat="server" style="margin-top: 0px" CssClass="auto-style15" TabIndex="5"></asp:TextBox>
-            <asp:CheckBox ID="masculino" runat="server" CssClass="auto-style17" TabIndex="7" Text="Masculino" />
-            <asp:CheckBox ID="femenino" runat="server" CssClass="auto-style18" TabIndex="8" Text="Femenino" />
+            <asp:CheckBox ID="masculino" runat="server" Checked="True" CssClass="auto-style17" OnCheckedChanged="masculino_CheckedChanged" TabIndex="7" Text="Masculino" AutoPostBack="True" />
+            <asp:CheckBox ID="femenino" runat="server" CssClass="auto-style18" OnCheckedChanged="femenino_CheckedChanged" TabIndex="8" Text="Femenino" AutoPostBack="True" />
             <asp:TextBox ID="telefono" runat="server" style="margin-top: 0px" CssClass="auto-style16" TabIndex="6"></asp:TextBox>
             <asp:Button ID="pagos" runat="server" CssClass="auto-style26" Text="Pagos" Width="100px" />
-            <asp:Button ID="salir" runat="server" CssClass="auto-style28" OnClick="salir_Click" Text="Salir" />
+            <asp:Button ID="salir" runat="server" CssClass="auto-style28" OnClick="salir_Click" Text="Salir" Height="22px" Width="100px" />
             <asp:Button ID="actividades" runat="server" CssClass="auto-style27" Text="Actividades" Width="100px" />
-            <asp:Button ID="alta" runat="server" CssClass="auto-style20" Text="Alta" Width="100px" />
+            <asp:Button ID="alta" runat="server" CssClass="auto-style20" OnClick="alta_Click" Text="Alta" Width="100px" />
             <asp:Button ID="baja" runat="server" CssClass="auto-style21" Text="Baja" Width="100px" />
             <asp:Button ID="modificar" runat="server" CssClass="auto-style22" Text="Modificar" Width="100px" />
             <asp:Button ID="hijos" runat="server" CssClass="auto-style23" Text="Hijos" Width="100px" />
-            <asp:Button ID="psicologo" runat="server" CssClass="auto-style24" Text="Psicólogo" Width="100px" />
-            <asp:Button ID="limpiar" runat="server" CssClass="auto-style25" Text="Limpiar campos" Width="100px" />
-            <asp:GridView ID="GridView1" runat="server" CssClass="auto-style29">
+            <asp:Button ID="psicologo" runat="server" CssClass="auto-style24" Text="Psicólogo" Width="100px" Height="22px" />
+            <asp:Button ID="limpiar" runat="server" CssClass="auto-style25" OnClick="limpiar_Click" Text="Limpiar campos" Width="100px" Height="22px" />
+            <asp:GridView ID="GridView1" runat="server" CssClass="auto-style29" AutoGenerateSelectButton="True" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
             </asp:GridView>
     </div>
     </form>
--- Prueba_Mierda_ASP/WebForM_Beneficiarios.aspx.cs
+++ Prueba_Mierda_ASP/WebForM_Beneficiarios.aspx.cs
@@ -4,18 +4,179 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Prueba_Mierda_ASP
 {
     public partial class WebForM_Beneficiarios : System.Web.UI.Page
     {
+        string sConection = "Data Source=localhost; User Id=MARILLAC; Password=Mvaldes1";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            cargarDatos();
+        }
+
+        protected void cargarDatos()
+        {
+            OracleConnection conexion = new OracleConnection();
+            conexion.ConnectionString = this.sConection;
+            conexion.Open();
+
+            string query = "SELECT * FROM BENEFICIARIO ORDER BY IDBENEFICIARIO";
+
+            OracleDataAdapter con = new OracleDataAdapter(query, conexion);
+            DataSet tabla = new DataSet();
+            con.Fill(tabla);
+            tabla.Tables[0].Columns.Remove("idPsicologo");
+            tabla.Tables[0].Columns[0].ColumnName = "Id";
+            tabla.Tables[0].Columns[1].ColumnName = "Nombre";
+            tabla.Tables[0].Columns[2].ColumnName = "Apellido paterno";
+            tabla.Tables[0].Columns[3].ColumnName = "Apellido materno";
+            tabla.Tables[0].Columns[4].ColumnName = "Dirección";
+            tabla.Tables[0].Columns[5].ColumnName = "Colonia";
+            tabla.Tables[0].Columns[6].ColumnName = "Teléfono";
+            tabla.Tables[0].Columns[7].ColumnName = "Sexo";
+            tabla.Tables[0].Columns[8].ColumnName = "Fecha de nacimiento";
+            tabla.Tables[0].Columns[9].ColumnName = "Edad";
+            GridView1.DataSource = tabla.Tables[0].DefaultView;
+            GridView1.DataBind();
         }
 
         protected void salir_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Menu.aspx");
+        }
+
+        protected void alta_Click(object sender, EventArgs e)
+        {
+            if(nombre.Text!="" && paterno.Text!="" && materno.Text!="" && direccion.Text!="" && colonia.Text!="" && telefono.Text!="" && fechaNacimiento.Text!="")
+            {
+                try
+                {
+                    OracleConnection m = new OracleConnection();
+                    OracleCommand cmd;
+                    string query;
+                    m.ConnectionString = sConection;
+                    m.Open();
+                    string sexo;
+                    if (masculino.Checked == true)
+                        sexo = "M";
+                    else
+                        sexo = "F";                                    
+                    query = "INSERT INTO beneficiario(nombre, paterno, materno, direccion, colonia, telefono, sexo, fechaNacimiento)"+
+                    " VALUES('"+nombre.Text+"', '"+paterno.Text+"', '"+materno.Text+"', '"+direccion.Text+"', '"+colonia.Text+"', "+telefono.Text+", '"+sexo+"', '"+fechaNacimiento.Text+"')";
+                    cmd = new OracleCommand(query, m);
+                    cmd.ExecuteNonQuery();
+                    cargarDatos();
+                    limpiarDatos(); 
+                }
+                catch (Exception ex) { Page.ClientScript.RegisterStartupScript(this.GetType(), "alert" + UniqueID, "alert('" + ex + "');", true); }
+            }
+            else
+            {
+                string script = "alert(\"No se pueden dejar campos vacíos.\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+            }
+                
+            
+        }
+
+        protected void limpiarDatos()
+        {
+            nombre.Text = "";
+            paterno.Text = "";
+            materno.Text = "";
+            direccion.Text = "";
+            colonia.Text = "";
+            telefono.Text = "";
+            masculino.Checked = true;
+            fechaNacimiento.Text = "";
+            femenino.Checked = false;
+        }
+
+        protected void limpiar_Click(object sender, EventArgs e)
+        {
+            limpiarDatos();
+        }
+
+        protected void masculino_CheckedChanged(object sender, EventArgs e)
+        {
+            if (masculino.Checked == true)
+                femenino.Checked = false;
+            else
+                femenino.Checked = true;   
+        }
+
+        protected void femenino_CheckedChanged(object sender, EventArgs e)
+        {
+            if (femenino.Checked == true)
+                masculino.Checked = false;
+            else
+                masculino.Checked = true;
+        }
+
+        protected void fechaNacimiento_TextChanged(object sender, EventArgs e)
+        {
+            bool limpiar = false;
+            if (fechaNacimiento.Text.Length != 10)
+                limpiar = true;
+            else
+            {
+                if (fechaNacimiento.Text[0] < 48 || fechaNacimiento.Text[0] > 51)
+                    limpiar = true;
+                if (fechaNacimiento.Text[2] != '-' || fechaNacimiento.Text[5] != '-')
+                    limpiar = true;
+                if (fechaNacimiento.Text[1] < 48 || fechaNacimiento.Text[1] > 57)
+                    limpiar = true;
+                if (fechaNacimiento.Text[3] != '0' && fechaNacimiento.Text[3] != '1')
+                    limpiar = true;
+                if (fechaNacimiento.Text[4] < 48 || fechaNacimiento.Text[4] > 57)
+                    limpiar = true;
+                if (fechaNacimiento.Text[6] < 48 || fechaNacimiento.Text[6] > 57 || fechaNacimiento.Text[7] < 48 || fechaNacimiento.Text[7] > 57 || fechaNacimiento.Text[8] < 48 || fechaNacimiento.Text[8] > 57 || fechaNacimiento.Text[9] < 48 || fechaNacimiento.Text[9] > 57)
+                    limpiar = true;
+            }
+
+            if (limpiar == true)
+                fechaNacimiento.Text = "";
+            
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = GridView1.SelectedIndex;
+            nombre.Text= GridView1.Rows[index].Cells[2].Text;
+            paterno.Text = GridView1.Rows[index].Cells[3].Text;
+            materno.Text = GridView1.Rows[index].Cells[4].Text;
+            direccion.Text= GridView1.Rows[index].Cells[5].Text;
+            colonia.Text = GridView1.Rows[index].Cells[6].Text;
+            telefono.Text = GridView1.Rows[index].Cells[7].Text;
+
+            string fecha = "";
+            fecha += GridView1.Rows[index].Cells[9].Text[0].ToString();
+            fecha += GridView1.Rows[index].Cells[9].Text[1].ToString();
+            fecha += "-";
+            fecha += GridView1.Rows[index].Cells[9].Text[3].ToString();
+            fecha += GridView1.Rows[index].Cells[9].Text[4].ToString();
+            fecha += "-";
+            fecha += GridView1.Rows[index].Cells[9].Text[6].ToString();
+            fecha += GridView1.Rows[index].Cells[9].Text[7].ToString();
+            fecha += GridView1.Rows[index].Cells[9].Text[8].ToString();
+            fecha += GridView1.Rows[index].Cells[9].Text[9].ToString();
+            fechaNacimiento.Text = fecha;
+
+            if(GridView1.Rows[index].Cells[8].Text=="M")
+            {
+                masculino.Checked = true;
+                femenino.Checked = false;
+            }
+            else
+            {
+                masculino.Checked = false;
+                femenino.Checked = true;
+            }
+
         }
     }
 }
